910172e39915b776d1c33c71d5c11cdafbeff983
calimameh <alimameh.ceesay@qlerify.com>
2025-06-12 23:40:03 +0200

    fix: update interfaces structure

--- nodejs/src/routes/index.js
+++ nodejs/src/routes/index.js
@@ -3,18 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const router = express.Router();
-const featuresPath = path.join(__dirname, '../interfaces/http/controllers');
+const controllersPath = path.join(__dirname, '../interfaces/http/controllers');
 
-fs.readdirSync(featuresPath).forEach((feature) => {
-  const featurePath = path.join(featuresPath, feature);
+fs.readdirSync(controllersPath).forEach((file) => {
+  const controller = require(path.join(controllersPath, file));
 
-  if (fs.lstatSync(featurePath).isDirectory()) {
-    fs.readdirSync(featurePath).forEach((file) => {
-      const controller = require(path.join(featurePath, file));
-      if (controller.router && controller.routeBase) {
-        router.use(controller.routeBase, controller.router);
-      }
-    });
+  if (controller.router && controller.routeBase) {
+    router.use(controller.routeBase, controller.router);
   }
 });
 
